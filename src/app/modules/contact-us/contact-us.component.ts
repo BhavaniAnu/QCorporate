470f23c48749e9e870560fde4de09a3f332f93f0
Bhavani <anubhavani5@gmail.com>
2022-01-27 19:55:50 +0530

    added contact form validations

--- src/app/modules/contact-us/contact-us.component.ts
+++ src/app/modules/contact-us/contact-us.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact-us',
@@ -11,38 +12,40 @@ export class ContactUSComponent implements OnInit {
   submitted = false;
 
   constructor(private formBuilder: FormBuilder) {
-    this.createContactForm();
   }
   
   ngOnInit(): void {
+    this.createContactForm();
   }
 
-  get f(): { [key: string]: AbstractControl } {
-    return this.contactForm.controls;
-  }
+  get f() { return this.contactForm.controls; }
 
   createContactForm(){
     const emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
     this.contactForm = this.formBuilder.group({
       username: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]], 
       email: ['', [Validators.required, Validators.pattern(emailPattern)]],
-      mobileno: ['', [Validators.required]],
-      message: [''],
+      mobileno: ['', [Validators.required, Validators.pattern("^[0-9]*$"), Validators.minLength(8)]],
+      message: ['', [Validators.maxLength(300)]],
     });
   }
 
   onSubmit() {
     this.submitted = true;
+    console.log('Your form data : ', this.contactForm.value );
     if (this.contactForm.invalid) {
       return;
     }
-    console.log('Your form data : ', this.contactForm.value );
     // emailjs.send('service_6ji0048', 'template_er6c1si', this.contactForm.value, 'user_PXF3H4e2PM6sdvC7RUuvr')
     // .then((res:EmailJSResponseStatus) => {
     //    console.log('success', res);
+    // this.submitted = false;
+    // this.contactForm.reset();
     // }, (err) => {
     //   console.log(err);
     // })
+    this.contactForm.reset();
+    this.submitted = false;
   }
 
 }
